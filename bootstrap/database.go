41bb13d170f36339f0b0c9965d15e5d19f53dab6
majun <645705505@qq.com>
2023-03-15 23:08:12 +0800

    数据库使用配置文件初始化

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -2,19 +2,90 @@ package bootstrap
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"time"
 
+	"github.com/MjSteed/vue3-element-admin-go/common"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 // Database 在中间件中初始化mysql链接
 func InitDatabase() *gorm.DB {
+	dbConfig := common.Config.Database
+
+	if dbConfig.Database == "" {
+		return nil
+	}
+	dsn := dbConfig.UserName + ":" + dbConfig.Password + "@tcp(" + dbConfig.Host + ":" + strconv.Itoa(dbConfig.Port) + ")/" +
+		dbConfig.Database + "?charset=" + dbConfig.Charset + "&parseTime=True&loc=Local"
 	mysqlConfig := mysql.Config{
-		DSN: "root:123456@(127.0.0.1:3306)/test?charset=utf8&parseTime=True&loc=Local", // DSN data source name
+		DSN:                       dsn,   // DSN data source name
+		DefaultStringSize:         50,    // string 类型字段的默认长度
+		DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: false, // 根据版本自动配置
 	}
-	db, err := gorm.Open(mysql.New(mysqlConfig))
-	if err != nil {
-		panic(fmt.Errorf("connnect to database failed: %s ", err))
+	if db, err := gorm.Open(mysql.New(mysqlConfig), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,            // 禁用自动创建外键约束
+		Logger:                                   getGormLogger(), // 使用自定义 Logger
+	}); err != nil {
+		panic(fmt.Errorf("mysql connect failed failed: %s ", err))
+	} else {
+		sqlDB, _ := db.DB()
+		sqlDB.SetMaxIdleConns(dbConfig.MaxIdleConns)
+		sqlDB.SetMaxOpenConns(dbConfig.MaxOpenConns)
+		return db
 	}
-	return db
+}
+
+func getGormLogger() logger.Interface {
+	var logMode logger.LogLevel
+
+	switch common.Config.Database.LogMode {
+	case "silent":
+		logMode = logger.Silent
+	case "error":
+		logMode = logger.Error
+	case "warn":
+		logMode = logger.Warn
+	case "info":
+		logMode = logger.Info
+	default:
+		logMode = logger.Info
+	}
+
+	return logger.New(getGormLogWriter(), logger.Config{
+		SlowThreshold:             200 * time.Millisecond,                      // 慢 SQL 阈值
+		LogLevel:                  logMode,                                     // 日志级别
+		IgnoreRecordNotFoundError: false,                                       // 忽略ErrRecordNotFound（记录未找到）错误
+		Colorful:                  !common.Config.Database.EnableFileLogWriter, // 禁用彩色打印
+	})
+}
+
+// 自定义 gorm Writer
+func getGormLogWriter() logger.Writer {
+	var writer io.Writer
+
+	// 是否启用日志文件
+	if common.Config.Database.EnableFileLogWriter {
+		// 自定义 Writer
+		writer = &lumberjack.Logger{
+			Filename:   common.Config.Logger.RootDir + "/" + common.Config.Database.LogFilename,
+			MaxSize:    common.Config.Logger.MaxSize,
+			MaxBackups: common.Config.Logger.MaxBackups,
+			MaxAge:     common.Config.Logger.MaxAge,
+			Compress:   common.Config.Logger.Compress,
+		}
+	} else {
+		// 默认 Writer
+		writer = os.Stdout
+	}
+	return log.New(writer, "\r\n", log.LstdFlags)
 }
